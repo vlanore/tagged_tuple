9f05fe40e73338862955b32b45b43d21e48dcb96
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-05-09 14:51:18 +0200

    Reorganized tagged_tuple.hpp

--- src/tagged_tuple.hpp
+++ src/tagged_tuple.hpp
@@ -30,6 +30,9 @@ license and that you accept its terms.*/
 #include "type_map.hpp"
 using std::string;
 
+//==================================================================================================
+// Tagged tuple class
+
 struct TaggedTupleTag {};
 
 template <class TagMap>
@@ -40,39 +43,65 @@ struct tagged_tuple_t : TaggedTupleTag {
 
     tuple_t data;
 
-    template <class... Args>
     // constructor that just perfect-forwards arguments to tuple constructor
+    template <class... Args>
     tagged_tuple_t(Args&&... args) : data(std::forward<Args>(args)...) {}
 
-    template <class Tag>
     // get a field of the tagged tuple by tag (returns a reference)
-    auto& get() {
-        constexpr int index = TagMap::template get_index<Tag>();
-        return std::get<index>(data);
-    }
-
     template <class Tag>
+    auto& get();
+
     // get a field of the tagged tuple by tag (returns a constant reference)
-    const auto& get() const {
-        constexpr int index = TagMap::template get_index<Tag>();
-        return std::get<index>(data);
-    }
+    template <class Tag>
+    const auto& get() const;
 
-    template <class First, class Second, class... Rest>
     // recursive version of getter (for tagged tuple parameters)
-    auto& get() {
-        return get<First>().template get<Second, Rest...>();
-    }
+    template <class First, class Second, class... Rest>
+    auto& get();
 
-    template <class First, class Second, class... Rest>
     // recursive version of getter (for tagged tuple parameters)
-    const auto& get() const {
-        return get<First>().template get<Second, Rest...>();
-    }
+    template <class First, class Second, class... Rest>
+    const auto& get() const;
 };
 
-// template <class Tag, class TTuple>
-// using type_of = std::get< TTuple::tag_map::
+//==================================================================================================
+// get element from tag
+
+// get a field of the tagged tuple by tag (returns a reference)
+template <class TagMap>
+template <class Tag>
+auto& tagged_tuple_t<TagMap>::get() {
+    constexpr int index = TagMap::template get_index<Tag>();
+    return std::get<index>(data);
+}
+
+// get a field of the tagged tuple by tag (returns a constant reference)
+template <class TagMap>
+template <class Tag>
+const auto& tagged_tuple_t<TagMap>::get() const {
+    constexpr int index = TagMap::template get_index<Tag>();
+    return std::get<index>(data);
+}
+
+// recursive version of getter (for tagged tuple parameters)
+template <class TagMap>
+template <class First, class Second, class... Rest>
+auto& tagged_tuple_t<TagMap>::get() {
+    return get<First>().template get<Second, Rest...>();
+}
+
+// recursive version of getter (for tagged tuple parameters)
+template <class TagMap>
+template <class First, class Second, class... Rest>
+const auto& tagged_tuple_t<TagMap>::get() const {
+    return get<First>().template get<Second, Rest...>();
+}
+
+//==================================================================================================
+// type_of
+
+//==================================================================================================
+// push_front
 
 namespace helper {
     template <class Tag, class TTuple, class Type, size_t... Is>
@@ -85,9 +114,9 @@ namespace helper {
     }
 };  // namespace helper
 
-template <class Tag, class Type, class TTuple>
 // adds a field to the struct and returns a new struct
-// WARNING: might invalidate old struct by moving its contents to new struct!
+// WARNING: might invalidate old struct by moving its contents to new struct
+template <class Tag, class Type, class TTuple>
 auto push_front(TTuple& t, Type&& new_data) {
     using underlying_tuple_t = typename TTuple::tuple_t;
 
@@ -97,8 +126,11 @@ auto push_front(TTuple& t, Type&& new_data) {
     return helper::push_front_helper<Tag>(t, std::forward<Type>(new_data), is);
 }
 
-template <class Tag, class Type>
+//==================================================================================================
+// tagged_tuple type creation
+
 // to be used in field list declaration
+template <class Tag, class Type>
 struct field {};
 
 namespace helper {
@@ -112,25 +144,28 @@ namespace helper {
     }
 };  // namespace helper
 
-template <class... Fields>
 // alias used to construct ttuple type from a list of fields (a list of "field" objects)
+template <class... Fields>
 using tagged_tuple = tagged_tuple_t<decltype(helper::map_from_fields(tuple<Fields...>()))>;
 
-template <class Tag, class Type>
+//==================================================================================================
+// make tagged tuple from values
+
 // to be used in make_tagged_tuple
+template <class Tag, class Type>
 struct TagValuePair {
     Type data;
     TagValuePair(Type&& data) : data(std::forward<Type>(data)) {}
 };
 
-template <class Tag, class Type>
 // to be used in make_tagged_tuple calls
+template <class Tag, class Type>
 auto field_from(Type&& data) {
     return TagValuePair<Tag, Type>(std::forward<Type>(data));
 }
 
-template <class Tag, class Type>
 // to be used in make_tagged_tuple calls; builds a unique pointer to Type from Type constructor args
+template <class Tag, class Type>
 auto unique_ptr_field(Type&& data) {
     return TagValuePair<Tag, std::unique_ptr<Type>>(
         std::make_unique<Type>(std::forward<Type>(data)));
@@ -146,11 +181,14 @@ namespace helper {
     }
 };  // namespace helper
 
-template <class... Fields>
 // builds a tuple from a list of fields with values (auto-deduces field types from values)
+template <class... Fields>
 auto make_tagged_tuple(Fields&&... fields) {
     return helper::make_tagged_tuple_helper(std::move(fields)...);
 }
+
+//==================================================================================================
+// is_tagged_tuple type trait
 
 template <class T>
 constexpr bool is_tagged_tuple = std::is_base_of<TaggedTupleTag, T>::value;
